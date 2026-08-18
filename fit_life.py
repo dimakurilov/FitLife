49b3c03b49b686b2d7a06e2b4a58bb76510b850f
Чернецкий Дмитрий <Dimakurilov2@yandex.ru>
2026-08-18 17:27:14 +0300

    Исправлен PEP8: убрана лишняя пустая строка

--- fit_life.py
+++ fit_life.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """FitLife - приложение для учёта физической активности."""
 
 name = input("Введите ваше имя: ")
@@ -8,15 +7,7 @@ height = float(input("Введите ваш рост (м): "))
 
 bmi = weight / (height ** 2)
 
-if bmi < 18.5:
-    category = "Недостаточный вес"
-elif 18.5 <= bmi < 25:
-    category = "Нормальный вес"
-elif 25 <= bmi < 30:
-    category = "Избыточный вес"
-else:
-    category = "Ожирение"
+water_intake = (weight * 30) / 1000
 
-print(f"{name}, ваш индекс массы тела: {bmi:.2f}")
-print(f"Категория: {category}")
-
+print(f"{name}, ваш индекс массы тела: {bmi}")
+print(f"{name}, рекомендуемая норма воды для вас (л/день): {water_intake}.")
