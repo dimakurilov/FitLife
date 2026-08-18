77bb4e8a3b5b1d120abd3c01a4c8e99d4a7113c6
Чернецкий Дмитрий <Dimakurilov2@yandex.ru>
2026-08-18 20:52:30 +0300

    Убрано форматирование у BMI и water_intake

--- fit_life.py
+++ fit_life.py
@@ -1,16 +1,16 @@
 """FitLife - приложение для учёта физической активности."""
 
-name = input("Введите ваше имя: ")
+user_name = input("Введите ваше имя: ")
 age = int(input("Введите ваш возраст: "))
-weight = float(input("Введите ваш вес (кг): "))
-height = float(input("Введите ваш рост (м): "))
+user_weight = float(input("Введите ваш вес (кг): "))
+user_height = float(input("Введите ваш рост (м): "))
 
-bmi = weight / (height ** 2)
+bmi = round(user_weight / (user_height ** 2), 1)
 
-water_intake = (weight * 30) / 1000
+water_intake = (user_weight * 30) / 1000
 
-print(f"{name}, ваш индекс массы тела: {bmi}")
-print(f"{name}, рекомендуемая норма воды для вас (л/день): {water_intake}.")
-
-
-
+print(f"Ваш индекс массы тела: {bmi}")
+print(
+    f"{user_name}, рекомендуемая норма воды для вас (л/день): "
+    f"{water_intake}.",
+)
